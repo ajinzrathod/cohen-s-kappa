ee59b8f5b7e2cbfb51440efc0d39d2ddd617227a
Ajinkya Rathod <57681532+ajinzrathod@users.noreply.github.com>
2021-09-10 13:07:51 +0530

    Update views.py

--- ck/tweet/views.py
+++ ck/tweet/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import auth
 from django.conf import settings as django_settings
-from django.utils.translation import activate
 from .models import Response, Tweet
 
 # Create your views here.
