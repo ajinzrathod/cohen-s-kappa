734b440baf9c5f828525f7bfc5b86747bf9f76be
ShivangiChotaliya <shivangichotaliya89@gmail.com>
2021-09-10 13:01:02 +0530

    display all tweets in  /tweet file by shivangi

--- ck/tweet/views.py
+++ ck/tweet/views.py
@@ -4,14 +4,19 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import auth
 from django.conf import settings as django_settings
-
+from django.utils.translation import activate
+from .models import Response, Tweet
 
 # Create your views here.
 
 
 @login_required
 def tweetsHome(request):
-    data = {
-        'active_page': 'tweets',
-    }
-    return render(request, 'tweets/index.html', data)
+    # data = {
+    #    'active_page': 'tweets',
+    # }
+
+    data = Response.objects.all()
+    return render(request,
+                  'tweets/index.html',
+                  {'data': data, 'active_page': 'tweets'})
