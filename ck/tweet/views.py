c8939fa47eee1875ee957112926213c677c392f9
Shivangi Chotaliya <75533966+ShivangiChotaliya@users.noreply.github.com>
2021-09-10 13:11:22 +0530

    Shivangi (#2)
    * initial commit by shivangi
    
    * display all tweets in  /tweet file by shivangi
    
    * Update views.py
    
    * Update views.py
    
    * Delete 1.ipynb
    
    Co-authored-by: Ajinkya Rathod <57681532+ajinzrathod@users.noreply.github.com>

--- ck/tweet/views.py
+++ ck/tweet/views.py
@@ -4,14 +4,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import auth
 from django.conf import settings as django_settings
-
+from .models import Response, Tweet
 
 # Create your views here.
 
 
 @login_required
 def tweetsHome(request):
-    data = {
-        'active_page': 'tweets',
-    }
-    return render(request, 'tweets/index.html', data)
+    data = Response.objects.all()
+    return render(request,
+                  'tweets/index.html',
+                  {'data': data, 'active_page': 'tweets'})
