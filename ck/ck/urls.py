e073db826a51d879547f19b8ff36c59c67987a8c
Ajinkya Rathod <rajinkya26@gmail.com>
2021-10-24 20:03:22 +0530

    Before Format Commit

--- ck/ck/urls.py
+++ ck/ck/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.utils.translation import ugettext_lazy
 
 import debug_toolbar
 
+admin.site.site_header = 'Admin'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
